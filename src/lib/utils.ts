c5e24d87a396a6747b7141f4401324ac487f05e1
Jake Nizio <jake.nizio2@gmail.com>
2025-06-14 14:09:02 -0400

    added hook for audio recording

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,19 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+// temp for testing
+export function playCombinedAudio(blobs: Blob[], mimeType = 'audio/webm') {
+  if (!blobs.length) return;
+
+  const combinedBlob = new Blob(blobs, { type: mimeType });
+  const url = URL.createObjectURL(combinedBlob);
+
+  const audio = new Audio(url);
+  audio.play();
+
+  audio.onended = () => {
+    URL.revokeObjectURL(url); // Clean up memory
+  };
+}
